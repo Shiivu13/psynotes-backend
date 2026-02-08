92b1acf786d01e22c0c4031caf5cbb1ff5b18f61
Shiivu13 <shivansh13703@gmail.com>
2026-02-08 16:04:54 +0530

    Revert: Undo SSL and Schema changes

--- utils/prismaClient.js
+++ utils/prismaClient.js
@@ -2,12 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { Pool } = require('pg');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: isProduction ? { rejectUnauthorized: false } : false
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({
     adapter,
